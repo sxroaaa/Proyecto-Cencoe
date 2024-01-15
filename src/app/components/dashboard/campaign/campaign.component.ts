3d75d09a07634360d92b1e61abebfa4b27047033
lcarvajalsa <110641945+lcarvajalsa@users.noreply.github.com>
2024-01-15 10:01:04 -0500

    estructura

---
+++ src/app/components/dashboard/campaign/campaign.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campaign',
+  standalone: true,
+  imports: [],
+  templateUrl: './campaign.component.html',
+  styleUrl: './campaign.component.scss'
+})
+export class CampaignComponent {
+
+}
---
+++ src/app/components/dashboard/cluster/cluster.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cluster',
+  standalone: true,
+  imports: [],
+  templateUrl: './cluster.component.html',
+  styleUrl: './cluster.component.scss'
+})
+export class ClusterComponent {
+
+}
